55c94ee86d2444d20d988acf7d4a47165da20237
OP-Aaditya <91126828+OP-Aaditya@users.noreply.github.com>
2023-06-29 13:33:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+video="";
+input="";
+
+function setup(){
+    canvas = createCanvas( 480 , 380 );
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(480 , 380);
+    video.hide();
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML= "Status: Object Detecting";
+    input = document.getElementById("input").value;
+}
+function modelLoaded(){
+    console.log("ModelLoaded!!!");
+    status = true;
+}
+function draw(){
+    img(video , 0 ,0 , 480 , 380 );
+}
